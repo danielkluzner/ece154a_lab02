8ce393358a27e421620d1e9de5e16a469cd1b7ee
Bliu00 <32946925+Bliu00@users.noreply.github.com>
2019-10-20 19:50:13 -0700

    Add files via upload

--- div.s
+++ div.s
@@ -2,17 +2,16 @@
 # File: div.s
 # Skeleton for ECE 154a project
 ##############################################################################
-
 	.data
 student:
-	.asciiz "Benjamin Liu, Daniel Kluzner" 	# Place your name in the quotations in place of Student
+	.asciiz "Student" 	# Place your name in the quotations in place of Student
 	.globl	student
 nl:	.asciiz "\n"
 	.globl nl
 
 
-op1:	.word 7				# divisor for testing
-op2:	.word 19			# dividend for testing
+op1:	.word 5				# divisor for testing
+op2:	.word 20			# dividend for testing
 
 
 	.text
@@ -58,6 +57,58 @@ divide:
 #	divu	$a1, $a0
 #	mflo	$a2
 #	mfhi	$a3
+
+addi $t8,$0,0      # counter for the loop
+addi $t9,$0,8      # limit for the loop
+
+# a0 = B
+# a1 = A
+
+add $s1,$0,$0     # R'
+add $s2,$0,$0     # R
+add $s3,$0,$0     # D
+add $s4,$0,$0     # Q
+addi $s5,$0,128      # 1 digit checker
+
+
+
+loop:
+    beq $t8, $t9, done
+    
+    sll $s1, $s1, 1 # shift R'
+    and $t7, $s5, $a1 # next bit of A
+    beq $t7, $s5, onebit
+        # if A-bit == 0
+        add $s2, $s1, $0
+        j skip1
+    onebit:
+        # if A-bit == 1
+        addi $s2, $s1, 1
+    skip1:
+    subu $s3, $s2, $a0 # D = R - B
+    slt $t5, $s3, $0
+    bne $t5, $0, yes
+        # if D >= 0
+        sll $s4, $s4, 1
+        addi $s4, $s4, 1
+        add $s1, $s3, $0
+        j skip2
+    yes: # if D < 0
+        sll $s4, $s4, 1
+        add $s1, $s2, $0
+    skip2:
+
+    srl $s5, $s5, 1 # shift digit checker
+
+    addi $t8, $t8, 1
+    j loop
+
+done: 
+    add $s2, $s1, $0 # R = R'
+    add $a3, $s2, $0
+    add $a2, $s4, $0
+
+
 ##############################################################################
 
 
